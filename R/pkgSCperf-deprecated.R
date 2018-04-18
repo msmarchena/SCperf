aeac4f25a84f954b32750f8be2ea1063349b9a1b
Marlene Silva Marchena <marchenamarlene@gmail.com>
2018-04-18 10:29:20 +0200

    Add files via upload

---
+++ R/pkgSCperf-deprecated.R
@@ -0,0 +1,13 @@
+#' @title Deprecated functions in the \pkg{SCperf} package
+#' 
+#' @description 
+#' The functions listed below are provided for compatibility 
+#' with older version of the \pkg{SCperf} package. They have been deprecated 
+#' and will be defunct in the next version of this package.
+#' Help pages for deprecated functions are available at 
+#' \code{help("-deprecated")}
+#' 
+#' @name pkgSCperf-deprecated
+#' @keywords internal
+NULL
+
